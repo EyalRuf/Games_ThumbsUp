eaa37f61ccc85b195cf584d4b5306ca5ad3ad6b6
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2020-09-16 11:17:44 +0200

    First draft Input system
    - Switched to new unity input system
    - Input system Debug mode working
    - Controller input most likely working but has not been tested

---
+++ ChaoticRockProject/Assets/Scripts/Input/ControllerManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerManager : MonoBehaviour
+{
+    public int connectedControllers;
+
+    private void Start()
+    {
+        connectedControllers = Gamepad.all.Count;
+    }
+
+    private void Update()
+    {
+        connectedControllers = Gamepad.all.Count;
+    }
+}
