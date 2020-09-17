180c8741747c8de38ec6c68a98f65d0e7ea85734
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2020-09-17 17:34:06 +0200

    Movement changes
    Made movement physics based.
    Made orthographic sample scene.
    Removed clutter

--- ChaoticRockProject/Assets/Scripts/PlayerMovement.cs
+++ ChaoticRockProject/Assets/Scripts/PlayerMovement.cs
@@ -2,22 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(StupidPlayerInput)), RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    StupidPlayerInput spi;
+    public float movementSpeed;
+    public float rotationSpeed;
 
-    public int speed = 10;
- 
-    void Start()
+    private Vector3 direction;
+    private Vector3 rotationDirection;
+
+    private StupidPlayerInput spi;
+    private Rigidbody rb;
+
+    private void Start()
     {
         spi = GetComponent<StupidPlayerInput>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void Update()
     {
-        Vector3 movement = new Vector3(-spi.controller.Joystick_Left.x, 0, spi.controller.Joystick_Left.y);      
+        //Old non physics movement
+        //Vector3 movement = new Vector3(spi.controller.Joystick_Left.x, 0, spi.controller.Joystick_Left.y);
+        //transform.position += movement * Time.deltaTime * movementSpeed;
+    }
 
-        transform.position += movement * Time.deltaTime * speed;
-        //GetComponent<Rigidbody>().MovePosition(transform.position + movement * Time.deltaTime * speed);
+    private void FixedUpdate()
+    {
+        //physics based movement
+        direction = new Vector3(spi.controller.Joystick_Left.x, 0, spi.controller.Joystick_Left.y);
+        rb.MovePosition(rb.position + direction * movementSpeed * Time.fixedDeltaTime);
+
+        //physics based rotation
+        Vector3 newRotationDirection = new Vector3(spi.controller.JoystickRaw_Left.x, 0, spi.controller.JoystickRaw_Left.y);
+        
+        //only rotate if stick is actually being pointed somewhere
+        if (newRotationDirection.magnitude != 0)
+        {
+            rotationDirection = newRotationDirection;
+
+            //calculate and rotate smoothly
+            Quaternion lookRotation = Quaternion.LookRotation(rotationDirection);
+            Quaternion smoothedRotation = Quaternion.Slerp(rb.rotation, lookRotation, rotationSpeed * Time.fixedDeltaTime);
+            rb.MoveRotation(smoothedRotation);
+        }
     }
 }
