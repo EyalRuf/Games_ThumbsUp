12980426f6abe98a6eacb33c77458b6d30cbbece
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2020-09-21 00:23:53 +0200

    Movement changes and initial demo scene
    - Added jumping, dashing stunning
    - created demo scene
    - imported image effects

---
+++ ChaoticRockProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(StupidPlayerInput)), RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("State")]
+    public bool stunned;
+    public bool dashing;
+    public bool grounded;
+
+    [Header("Stunning")]
+    public LayerMask floorMask;
+    public GameObject stunDecal;
+    public float stunDuration;
+    private float stunCounter = int.MinValue;
+
+    [Header("Default Movement")]
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float stayUprightSpeed;
+
+    [Header("Jumping")]
+    public float jumpingForce;
+    public Transform jumpCheckPoint;
+
+    [Header("Dashing")]
+    public float dashingKnockBack;
+    public float additionalDashSpeed;
+    public float dashDuration;
+    public float dashCooldown;
+    private float dashCounter;
+    private float dashCooldownCounter;
+
+    private Vector3 direction;
+    private Vector3 rotationDirection;
+
+    private StupidPlayerInput spi;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        spi = GetComponent<StupidPlayerInput>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //stunning state
+        stunCounter -= Time.deltaTime;
+        stunned = stunCounter > 0;
+
+        stunDecal.SetActive(stunned);
+
+        //grounded state
+        Collider[] colliders = Physics.OverlapSphere(jumpCheckPoint.position, 0.01f, floorMask);
+        grounded = colliders.Length > 0;
+
+        //jumping
+        if (grounded && spi.controller.ADown)
+        {
+            rb.AddForce(Vector3.up * jumpingForce, ForceMode.Acceleration);
+        }
+
+        //Dashing
+        dashCooldownCounter -= Time.deltaTime;
+        dashCounter -= Time.deltaTime;
+
+        dashing = dashCounter > 0;
+
+        if ((spi.controller.L1Down || spi.controller.L2Down) && dashCooldownCounter < 0.0f)
+        {
+            dashCooldownCounter = dashCooldown;
+            dashCounter = dashDuration;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //if not stunned
+        if (!stunned)
+        {
+            //keep player upright
+            Quaternion rot = Quaternion.FromToRotation(transform.up, Vector3.up);
+            rb.AddTorque(new Vector3(rot.x, rot.y, rot.z) * stayUprightSpeed * Time.fixedDeltaTime);
+
+
+
+            //physics based movement and dashing
+            direction = new Vector3(spi.controller.Joystick_Left.x, 0, spi.controller.Joystick_Left.y);
+            rb.MovePosition(
+                rb.position + 
+                    (
+                        (direction * movementSpeed + 
+                        (dashing ? (direction * additionalDashSpeed) : Vector3.zero)
+                    ) * Time.fixedDeltaTime)
+                );
+
+            //physics based rotation
+            Vector3 newRotationDirection = new Vector3(spi.controller.JoystickRaw_Left.x, 0, spi.controller.JoystickRaw_Left.y);
+
+            //only rotate if stick is actually being pointed somewhere
+            if (newRotationDirection.magnitude != 0)
+            {
+                rotationDirection = newRotationDirection;
+
+                //calculate and rotate smoothly
+                Quaternion lookRotation = Quaternion.LookRotation(rotationDirection);
+                Quaternion smoothedRotation = Quaternion.Slerp(rb.rotation, lookRotation, rotationSpeed * Time.fixedDeltaTime);
+
+                rb.MoveRotation(smoothedRotation);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //for stunning other players
+        if (dashing)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                PlayerController otherPlayer = collision.gameObject.GetComponent<PlayerController>();
+
+                if (!otherPlayer.stunned)
+                {
+                    //calculate knockback for other player
+                    Vector3 knockback = (collision.transform.position - transform.position).normalized * dashingKnockBack;
+
+                    //stun
+                    otherPlayer.Stun(knockback);
+                }
+            }
+            else if(collision.gameObject.CompareTag("Rock"))
+            {
+                //calculate knockback for rock
+                Vector3 knockback = (collision.transform.position - transform.position).normalized * dashingKnockBack;
+
+                //do knockback
+                collision.gameObject.GetComponent<Rigidbody>().AddForce(knockback, ForceMode.Acceleration);
+            }
+        }
+    }
+
+    public void Stun(Vector3 knockback)
+    {
+        stunCounter = stunDuration;
+        stunned = true;
+
+        rb.AddForce(knockback, ForceMode.Acceleration);
+    }
+}
