9d3bd0dcd3c5d7d5df1a46d233406442a44835b6
Eyal Rufeisen <eyalrocks@gmail.com>
2020-10-01 19:32:07 +0200

    Started creating animations & changed camera zoomspeed

---
+++ ChaoticRockProject/Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimations : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private bool isWalking;
+    [SerializeField]
+    private bool isCarrying;
+    [SerializeField]
+    private bool isJumping;
+
+    void Start()
+    {
+        isWalking = false;
+        isCarrying = false;
+        isJumping = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
